01f25c904b0bde659a80584cb836a3ed36f7e393
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-10 12:53:50 +0200

    10/10/2025

--- Repositorio/Unidad3/Practica2/ejer4.php
+++ Repositorio/Unidad3/Practica2/ejer4.php
@@ -1,3 +1,17 @@
 <?php
-    
+    $matriz = [];
+    for($i = 0; $i <4; $i++){
+        for($j = 0; $j <4; $j++){
+            $matriz[$i][$j] = rand(1,100);
+        }
+    }
+
+    echo "Matriz 4x4<br>";
+    for($i = 0; $i <4; $i++){
+        for($j = 0; $j <4; $j++){
+          //echo str_pad($matriz[$i][$j], 4, " ", STR_PAD_LEFT);
+            echo $matriz[$i][$j] ."\n";
+        }
+        echo "<br>";
+    }
 ?>
