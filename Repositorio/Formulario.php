e4e514a012daa3b967df795d5b399f91b7219f95
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-16 11:14:30 +0200

    16/10/2025

---
+++ Repositorio/Formulario.php
@@ -0,0 +1,21 @@
+<?php
+// Formulario
+    if (isset($_POST['name'])) //$name = $_POST['name'];
+    echo $_POST['name'];
+    // else $name = "(Not entered)";
+    echo <<<_END
+        <html>
+            <head>
+                <title>Form Test</title>
+            </head>
+            <body>
+                Your name is: $name<br>
+                <form method="post" action="formtest2.php">
+                    What is your name?
+                    <input type="text" name="name">
+                    <input type="submit">
+                </form>
+            </body>
+            </html>
+_END;
+?>
