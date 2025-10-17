38b09ebb913c9bc72f104ca2c362bbf2e32527a4
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-17 11:09:53 +0200

    17/10/2025

--- Repositorio/Unidad4/Formulario3.php
+++ Repositorio/Unidad4/Formulario3.php
@@ -1,43 +1,25 @@
-<?php
-    // isset comprueba si las variables existen y no son nulas
-    if(isset($_POST["num1"]) && isset($_POST["num2"]) && isset($_POST["oper"])){
-        // guarda los valores enviados en variables 
-        $num1 = $_POST["num1"];
-        $num2 = $_POST["num2"];
-        $oper = $_POST["oper"];
-
-        switch ($_POST["oper"]) {
-            case '+':
-                echo "La suma de " .$_POST["num1"] ." + " .$_POST["num2"] ." = " .($_POST["num1"] + $_POST["num2"]);
-                break;
-            case '-':
-                echo "La resta de " .$_POST["num1"] ." - " .$_POST["num2"] ." = " .($_POST["num1"] - $_POST["num2"]);
-                break;
-            case '*':
-                echo "La multiplicacion de " .$_POST["num1"] ." * " .$_POST["num2"] ." = " .($_POST["num1"] * $_POST["num2"]);
-                break;
-            case '/':
-                echo "La division de " .$_POST["num1"] ." / " .$_POST["num2"] ." = " .($_POST["num1"] / $_POST["num2"]);
-                break;
-            default:
-                break;
-        }
-    } else { // si las variables no existen, es decir si no se ejecuto 
-        echo <<<_END
-            <html>
-                <form action="Formulario3.php" method="post">   
-                    <label for="num1">Numero 1</label>
-                    <input type="num" id="num1" name="num1" placeholder="Ingresa el Numero 1:" required>
-
-                    <label for="num2">Numero 2</label>
-                    <input type="num" id="num2" name="num2" placeholder="Ingresa el Numero 2: " required>
-
-                    <label for="oper">Operador</label>
-                    <input type="text" id="oper" name="oper" placeholder="+, -, *, / " required>
-
-                    <button type="submit">Enviar</button>
-                </form>
-            </html>
-        _END;
-    }
-?>
+<!--Formulario Dinamico-->
+<!--
+    '' no interpreta variables
+    "" si interpreta variables
+-->
+<html>
+    <form action="Formulario3.php" method ="post">
+        <?php
+            // isset comprueba si las variables existen y no son nulas
+            if(isset($_POST['num'])){
+                $numeros = $_POST['num'];
+                foreach($numeros as $index => $valor){
+                    echo "Numero ".($index+1) . ": " .$valor ."<br>";
+                }
+            } else {
+                for($i = 1; $i <=10; $i++){
+                    echo '<label for="num' . $i . '">Número ' . $i . ':</label>';
+                    echo '<input type="number" id="num' . $i . '" name="num[]" placeholder="Ingresa el Número :" required><br>';
+                                                                //name="num[]" array de valores que se almacenan en $_POST['num']
+                }
+            }
+        ?>
+        <input type="submit" value="Enviar">
+    </form>
+</html>
