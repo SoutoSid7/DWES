b4eab8e706eebe1a604a0cc650cea5951c99995f
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-17 13:24:02 +0200

    17/10/2025

--- Repositorio/Unidad4/Formulario3B.php
+++ Repositorio/Unidad4/Formulario3B.php
@@ -2,7 +2,7 @@
     <form action ="Formulario3.php" method="post">
         <?php
         echo "Numero de Elementos: ";
-        echo '<input type="num" id="tope" name="tope"><br>';
+        echo '<input type="number" id="tope" name="tope"><br>';
         echo '<input type="submit" value="Enviar">';
         ?>
     </form>
