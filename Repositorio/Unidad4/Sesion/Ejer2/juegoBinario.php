787a6f27abdb4fcee412182a6b9bc10e9510ad0b
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-24 14:04:10 +0200

    24/10/2025

---
+++ Repositorio/Unidad4/Sesion/Ejer2/juegoBinario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juego Binario</title>
+</head>
+<body>
+    <h1>Adivina el Juego en Binario</h1>
+    <?php
+
+    ?>
+</body>
+</html>
