945f8cfbfe841442733dd4b8a40ab041057a8e05
mariosouto <mariosouto226@gmail.com>
2025-10-27 00:58:42 +0100

    25-10-2025

--- Repositorio/Unidad4/Sesion/Ejer2/juegoBinario.php
+++ Repositorio/Unidad4/Sesion/Ejer2/juegoBinario.php
@@ -8,11 +8,15 @@
 <body>
     <h1>Adivina el Juego en Binario</h1>
     <?php
+        session_start();
         $binario = [];
         for ($i = 0; $i <= 3; $i++) {
             $num = rand(0, 1);
             $binario[$i] = $num;
         }
+
+        $_SESSION["binario"] = $binario; //guarda en sesion
+
         echo "El número binario generado es: " . implode("", $binario) ."<br><br>"; // .implode devuelve los 4 numeros del array como un string
         foreach ($binario as $i => $bin){
             switch($i){
@@ -32,8 +36,11 @@
             echo "<img src='$img' width='80' height='100'> ";
         }
     ?>
-    <form action = "Formulario4.php" method = "post">
+    <form action = "binario.php" method = "post">
+        <label for="num">Introduce el numero en decimal:</label>
+        <input type ="number" name="num"><br>
 
+        <input type="submit" value="Comprobar">
     </form>
 </body>
 </html>
