1e066016e5260f5ef803d6151c8cb71326b44e94
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-27 13:00:23 +0100

    27/10/2025

--- Repositorio/Unidad4/Sesion/Ejer3/jugar.php
+++ Repositorio/Unidad4/Sesion/Ejer3/jugar.php
@@ -13,11 +13,15 @@
 
         pintarCirculos ("black", "black", "black", "black"); // pinta los circulos de negro en la funcion 
 
+        
     ?>
 
+    <p>Pulsa los Botones en el Orden Correcto: </p>
     <form action="jugar.php" method="post">
-        
+        <button type="submit" name="red" style ="background-color:red; color:white; width:75px; height:35px; border-radius: 2px">ROJO</button>
+        <button type="submit" name="blue" style ="background-color:blue; color:white; width:75px; height:35px; border-radius: 2px">AZUL</button>
+        <button type="submit" name="yellow" style ="background-color:yellow; color:black; width:80px; height:35px; border-radius: 2px">AMARILLO</button>
+        <button type="submit" name="green" style ="background-color:green; color:white; width:75px; height:35px; border-radius: 2px">VERDE</button>
     </form>
-
 </body>
 </html>
