75afecac2b6dc236833e3b9687ede88d9837376d
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-10-27 09:40:41 +0100

    27/10/2025

--- Repositorio/Unidad4/Sesion/Ejer2/binario.php
+++ Repositorio/Unidad4/Sesion/Ejer2/binario.php
@@ -1,7 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
     session_start();
     $_SESSION["num"] = $_POST["num"] ;
-    echo "El numero introducido fue: " .$_SESSION["num"] ."<br>";
 
     // Recupera el binario desde la sesion
     $binario = $_SESSION["binario"];
@@ -10,9 +17,11 @@
     $decimal = bindec($binarioN); // convierte el binario a decimal 
 
     if($_SESSION["num"] == $decimal){
-        echo "Correcto";
+        echo "Correcto, el numero ".$_SESSION ["num"]. " en binario es: ". $binarioN;
     } else{
-        echo "Incorrecto";
+        echo "Incorrecto, el numero ".$_SESSION["num"]. " NO es en binario ".$binarioN .", el numero correcto es: ".$decimal;
     }
-
-?>
+    ?>
+    <br><a href="juegoBinario.php" title="Ir la página anterior">Volver</a>
+</body>
+</html>
