c04ef701d666a49b20e182bb34b1b50a4b7ff2b0
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-11-06 10:35:22 +0100

    06/11/2025

---
+++ Repositorio/Unidad4/Sesion/Ejer3/v4/fallo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simon</title>
+</head>
+<body>
+    <h1>Simón</h1>
+    <?php
+        session_start();
+        require_once 'pintarCirculos.php';
+
+        $solucion = $_SESSION["solucion"];
+        $intentos = $_SESSION["intentos"] ?? [];
+    ?>
+
+    <p>La secuencia correcta es:</p>
+    <?php 
+        pintarCirculos($solucion[0], $solucion[1], $solucion[2], $solucion[3]);
+    ?>
+
+    <p>Tu secuencia fue:</p>
+    <?php 
+        pintarCirculos($intentos[0], $intentos[1], $intentos[2], $intentos[3]);
+    ?>
+
+    <form action="inicio.php" method="post">
+        <input type="submit" value="Volver a jugar">
+    </form>
+    
+</body>
+</html>
