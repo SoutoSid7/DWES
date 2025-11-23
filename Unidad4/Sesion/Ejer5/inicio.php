402132343fab563aad6ffd3d465e56e48a8e0af1
mariosouto <mariosouto226@gmail.com>
2025-11-24 00:56:57 +0100

    Remove embedded git repository

--- Unidad4/Sesion/Ejer5/inicio.php
+++ Unidad4/Sesion/Ejer5/inicio.php
@@ -9,7 +9,19 @@
     <h1>AGENDA</h1>
     <?php
         session_start();
-        echo "Hola " .$_SESSION["nombre"];
+        echo "Hola " .$_SESSION["nombre"] ." cuantos contactos deseas grabar?";
+        echo "<br>Puedes grabar entre 1 y 5. Por cada pulsacion en INCREMENTAR grabaras un usario mas. ";
+        echo "<br>Cuando el número sea el deseado pulsa GRABAR";
+
+        $img[0] = './imagenes/OIP0.jfif'; 
+        $img[1] = './imagenes/OIP1.jfif'; 
+        $img[2] = './imagenes/OIP2.jfif'; 
+        $img[3] = './imagenes/OIP3.jfif'; 
+        $img[4] = './imagenes/OIP4.jfif'; 
+
+        $i = rand(0,4);
+
+        echo "<br>$i";
     ?>
 </body>
 </html>
