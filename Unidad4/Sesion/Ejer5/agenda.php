980f450e5dded6e405f6e88b0b9717df04b1f66d
mariosouto <mariosouto226@gmail.com>
2025-11-24 13:04:33 +0100

    EjExamen

--- Unidad4/Sesion/Ejer5/agenda.php
+++ Unidad4/Sesion/Ejer5/agenda.php
@@ -7,5 +7,34 @@
 </head>
 <body>
     <h1>Agenda</h1>
+    <?php
+        session_start();
+        // Datos De Conexión
+        $hn = 'localhost';
+        $db = 'agenda';
+        $un = 'root';
+        $pw = '';
+
+        $conn = new mysqli($hn, $un, $pw, $db);
+        if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
+
+        echo "Hola " .$_SESSION["nombre"];
+
+        ?>
+        <form method="post">
+            <?php
+                // Formularios Segun Numero De Incrementos
+                for($i = 0; $i < $_SESSION["contador"]; $i++){
+                    echo '<form method="post">';
+                    echo '<h3>Contacto' . ($i + 1) . '</h3>';
+                    echo 'Nombre: <input type="text" name="nombre_' . $i . '"<br>';
+                    echo 'Email: <input type="email" name="email' .$i . '"<br>';
+                    echo 'Teléfono: <input type="text" name="telefono_' . $i . '"<br>';
+                    echo '</form>';
+                }
+            ?>
+            <br>
+            <input type="submit" name="grabar" value="Grabar">
+        </form>
 </body>
 </html>
