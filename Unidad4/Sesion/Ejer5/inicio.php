10602cf70b3cf4b4564807c0f4cfe38dad9dbb68
mariosouto <mariosouto226@gmail.com>
2025-11-24 12:40:44 +0100

    EjExamen

--- Unidad4/Sesion/Ejer5/inicio.php
+++ Unidad4/Sesion/Ejer5/inicio.php
@@ -9,33 +9,56 @@
     <h1>Agenda</h1>
     <?php
         session_start();
-        echo "Hola " .$_SESSION["nombre"] ." cuantos contactos deseas grabar?";
-        echo "<br>Puedes grabar entre 1 y 5. Por cada pulsacion en INCREMENTAR grabaras un usario mas. ";
-        echo "<br>Cuando el número sea el deseado pulsa GRABAR<br><br>";
-
+        // Reinicia El Contador
+        if($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $_SESSION['contador'] = 1;
+        }
+        
+        // Array De Imagenes
         $img[0] = './imagenes/OIP0.jfif'; 
         $img[1] = './imagenes/OIP1.jfif'; 
         $img[2] = './imagenes/OIP2.jfif'; 
         $img[3] = './imagenes/OIP3.jfif'; 
         $img[4] = './imagenes/OIP4.jfif'; 
-        $i = rand(0,4);
 
-        echo '<img src="' . $img[$i] . '" alt="Descripción de la imagen" width="100" height="100">';
+        // Inicializando Contador, Empieza Mostrando 1 Imagen
+        if(!isset($_SESSION["contador"])){
+            $_SESSION["contador"] = 1;
+        }
 
+        // Cada Vez Que Se Incremente Y Sea Menos De 5 El Contador Suma
         if(isset($_POST["incrementar"])){
             if($_SESSION["contador"] < 5){
                 $_SESSION["contador"]++;
             }
         }
 
-        for ($i = 0; $i < $_SESSION["contador"]; $i++){
+        // Cuando Le De A Grabar, Pase De Pagina
+        if(isset($_POST["grabar"])){
+            header("Location: agenda.php");
+            exit;
+        }
+        // Si Llega A 5 Tambien Pasa
+        if($_SESSION["contador"]==5){
+            header("Location: agenda.php");
+            exit;           
+        }
+
+        echo "Hola " .$_SESSION["nombre"] ." cuantos contactos deseas grabar?";
+        echo "<br>Puedes grabar entre 1 y 5. Por cada pulsacion en INCREMENTAR grabaras un usario mas. ";
+        echo "<br>Cuando el número sea el deseado pulsa GRABAR<br><br>";
+
+        // Bucle Imprimir Imagenes
+        for($i = 0; $i < $_SESSION["contador"]; $i++){
             $indice = rand(0,4);
             echo '<img src="' . $img[$indice] . '" alt="Descripción de la imagen" width="100" height="100">';
         }
 
     ?>
     <br><br>
-    <input type="submit" name="incrementar" value="INCREMENTAR">
-    <input type="submit" name="grabar" value="GRABAR"> 
+    <form method="post">
+        <input type="submit" name="incrementar" value="INCREMENTAR">
+        <input type="submit" name="grabar" value="GRABAR"> 
+    </form>
 </body>
 </html>
