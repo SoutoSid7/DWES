71c3230339eaf51aab70f335d9e036f887158df5
mariosouto <mariosouto226@gmail.com>
2025-11-24 23:12:38 +0100

    EjExamen

--- Unidad4/Sesion/Ejer5/agenda.php
+++ Unidad4/Sesion/Ejer5/agenda.php
@@ -19,22 +19,50 @@
         if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
 
         echo "Hola " .$_SESSION["nombre"];
-
-        ?>
-        <form method="post">
-            <?php
-                // Formularios Segun Numero De Incrementos
-                for($i = 0; $i < $_SESSION["contador"]; $i++){
-                    echo '<form method="post">';
-                    echo '<h3>Contacto' . ($i + 1) . '</h3>';
-                    echo 'Nombre: <input type="text" name="nombre_' . $i . '"<br>';
-                    echo 'Email: <input type="email" name="email' .$i . '"<br>';
-                    echo 'Teléfono: <input type="text" name="telefono_' . $i . '"<br>';
-                    echo '</form>';
+    ?>
+    <form method="post">
+        <?php
+            for ($i = 0; $i < $_SESSION["contador"]; $i++) {
+                echo "<h3>Contacto " . ($i + 1) . "</h3>";
+                echo 'Nombre: <input type="text" name="nombre'.$i.'"><br>';
+                echo 'Email: <input type="email" name="email'.$i.'"><br>';
+                echo 'Teléfono: <input type="text" name="telefono'.$i.'"><br><br>';
+            }
+    
+            // Obtener codigo del usuario logeado
+            $sql = "SELECT Codigo FROM usuarios WHERE Nombre = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt-> bind_param("s", $_SESSION["nombre"]);
+            $stmt-> execute();
+    
+            $resultado = $stmt->get_result();
+            $usuario = $resultado->fetch_assoc(); // convierte una fila de la consulta en un array asociativo
+            
+            $codUsuario = $usuario["Codigo"];
+    
+            // Procesar grabacion
+            if(isset($_POST["grabar"])){
+                for($i=0; $i < $_SESSION["contador"]; $i++){
+                    $nombre = $_POST["nombre$i"] ?? "";
+                    $email = $_POST["email$i"] ?? "";
+                    $telefono = $_POST["telefono$i"] ?? "";
+    
+                    $sql = "INSERT INTO contactos (nombre, email, telefono, codusuario)
+                            VALUES (?, ?, ?, ?)";
+    
+                    $stmtInsert = $conn->prepare($sql);
+                    $stmtInsert-> bind_param("sssi", $nombre, $email, $telefono, $codUsuario);
+                    
+                    if($stmtInsert->execute()){
+                        echo "<br>Datos Guardados Correctamente";
+                    } else {
+                        echo "<br>Error " .$stmtInsert->error;
+                    }
                 }
-            ?>
-            <br>
-            <input type="submit" name="grabar" value="Grabar">
-        </form>
+            }
+        ?>
+        <br>
+        <input type="submit" name="grabar" value="Grabar">
+    </form>
 </body>
 </html>
