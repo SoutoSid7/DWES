0fb1b28eb2ade1682d774a946a079adb629d5751
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-12-05 10:45:20 +0100

    Ejer6

--- Unidad4/Sesion/Ejer6/resultado.php
+++ Unidad4/Sesion/Ejer6/resultado.php
@@ -10,6 +10,38 @@
 
     $conn = new mysqli($hn, $un, $pw, $db);
     if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
+    
+    // Jugadores Acertantes NUMERO
+    $sqlNumAciertos = "SELECT COUNT(*) AS acertantes
+            FROM respuestas r
+            JOIN solucion s ON r.fecha = s.fecha
+            WHERE r.fecha = CURDATE()
+                AND r.respuesta = s.solucion;
+            ";
+    $acertantes = $conn->query($sqlNumAciertos);
+    $filaAciertos = $acertantes->fetch_assoc();
+    $num_acertantes = $filaAciertos['acertantes'] ?? 0;
+
+    // Jugadores Acertantes NOMBRE & HORA
+    $sqlNombreAcertantes = "SELECT j.nombre, r.hora
+            FROM respuestas r
+            JOIN jugador j ON r.login = j.login
+            JOIN solucion s ON r.fecha = s.fecha
+            WHERE r.fecha = CURDATE()
+                AND r.respuesta = s.solucion
+            ORDER BY r.hora;
+            ";
+    $nombreAciertos = $conn->query($sqlNombreAcertantes);
+
+    // Jugadores Fallan NOMBRE & HORA
+    $sqlNombreFallo = "SELECT j.nombre, r.hora
+            FROM respuestas r
+            JOIN jugador j ON r.login = j.login
+            JOIN solucion s ON r.fehca = s.fecha
+            WHERE r.fecha = CUERDATE()
+                AND r.respuesta <> s.solucion
+            ORDER BY r.hora;";
+    $fallos = $conn->query($sqlNombreFallo)
 ?>
 
 <!DOCTYPE html>
@@ -21,5 +53,6 @@
 </head>
 <body>
     <h1>Fecha <?php echo date("d-m-Y"); ?></h1>
+    <h3>Numero de jugadores acertantes: <?php echo $num_acertantes; ?> </h3>
 </body>
 </html>
