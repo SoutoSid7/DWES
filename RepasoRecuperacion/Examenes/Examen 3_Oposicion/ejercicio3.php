f50cf289da759d93045bdca49eb4dae9a939c47f
mariosouto <mariosouto226@gmail.com>
2026-04-22 00:05:38 +0200

    Repaso

--- RepasoRecuperacion/Examenes/Examen 3_Oposicion/ejercicio3.php
+++ RepasoRecuperacion/Examenes/Examen 3_Oposicion/ejercicio3.php
@@ -1,5 +1,57 @@
 <?php
     session_start();
+
+    if(isset($_POST["guardar"])){
+        $dni = $_SESSION["dni"] ?? '';        
+        $codCurso = $_POST["codCurso"] ?? '';
+        $pruebaA = $_POST["pruebaA"] ?? '';
+        $pruebaB = $_POST["pruebaB"] ?? '';
+        $tipo = $_POST["tipo"] ?? '';
+        $inscripcion = $_POST["inscripcion"] ?? '';
+
+        // Datos de Conexion
+        $hn = 'localhost';
+        $db = 'oposicion';
+        $un = 'root';
+        $pw = '';
+
+        $conn = new mysqli($hn, $un, $pw, $db);
+        if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
+        
+        // Codigo Curso Comprobar
+        $sqlCodCurso = "
+                SELECT * 
+                FROM curso
+                WHERE codigocurso = ?;
+            ";
+
+        $stmt = $conn->prepare($sqlCodCurso);
+        $stmt-> bind_param("i", $codCurso);
+        $stmt->execute();
+    
+        $resultado = $stmt->get_result();
+
+        if($resultado->num_rows == 0){
+            echo("El curso no existe");
+        }
+
+        // Crear Matricula
+        $fecha = date("Y-m-d", strtotime($inscripcion));
+
+        $sqlMatricula = "
+                INSERT INTO matricula(dnialumno, codcurso, pruebaA, pruebaB, tipo, inscripcion) 
+                VALUES (?, ?, ?, ?, ?, ?);
+        ";
+        $stmt = $conn->prepare($sqlMatricula);
+        $stmt-> bind_param("ssssss", $dni, $codCurso, $pruebaA, $pruebaB, $tipo, $inscripcion);
+        $stmt->execute();
+
+        if($stmt->affected_rows > 0){
+            echo "La matricula del alumno " .$dni ." en el curso " .$codCurso ." se ha realizado correctamente"; 
+        } else {
+            echo "Fatal ERROR";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +69,11 @@
 
     <form action="ejercicio3.php" method="post" >
         <label for="dni">DNI:</label>
-        <input type="text" name="dni" value="<?php echo $_SESSION['dni']; ?>" readonly>
+        <span>
+            <?php
+                echo $_SESSION["dni"];
+            ?>
+        </span>
         <br>
         <label for="codCurso">Cod Curso:</label>
         <input type="text" id="codCurso" name="codCurso">
@@ -32,10 +88,10 @@
         <input type="text" id="tipo" name="tipo">
         <br>
         <label for="inscripcion">Inscripcion:</label>
-        <input type="text" id="inscripcion" name="inscripcion">
+        <input type="date" id="inscripcion" name="inscripcion">
 
         <br><br>
-        <input type="submit" name="entrar" value="Guardar"><br><br>
+        <input type="submit" name="guardar" value="Guardar"><br><br>
     </form>
 
 </body>
